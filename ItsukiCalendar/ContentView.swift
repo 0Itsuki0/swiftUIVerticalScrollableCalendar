afd010fda1118763bc580ce5f8661a0f2a00f831
000itsuki000 <itsuki.h@collabo-style.co.jp>
2024-04-06 09:46:26 +0900

    Initial Commit

---
+++ ItsukiCalendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ItsukiCalendar
+//
+//  Created by Itsuki on 2024/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ItsukiCalendar/ItsukiCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsukiCalendarApp.swift
+//  ItsukiCalendar
+//
+//  Created by Itsuki on 2024/04/06.
+//
+
+import SwiftUI
+
+@main
+struct ItsukiCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
